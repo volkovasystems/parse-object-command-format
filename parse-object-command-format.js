49e7e0d1e4bf4dea9bc173f27d079fb6ab822889
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-06-26 21:57:54 +0800

    Added parseObjectCommandFormat procedure.

---
+++ parse-object-command-format.js
@@ -0,0 +1,13 @@
+try{ var base = window; }catch( error ){ var base = exports; }
+( function module( base ){
+	define( "parseObjectCommandFormat",
+		[
+		],
+		function construct( ){
+			var parseObjectCommandFormat = function parseObjectCommandFormat( data ){
+				
+			};
+
+			base.parseObjectCommandFormat = parseObjectCommandFormat;
+		} );
+} )( base );
