145e49f7e3597a360b5bff8af98b5ce5aaed764c
Richeve S. Bebedor <volkovasystems@gmail.com>
2014-07-16 17:02:58 +0800

    Completed parseObjectCommandFormat procedure.

--- parse-object-command-format.js
+++ parse-object-command-format.js
@@ -1,13 +1,59 @@
-try{ var base = window; }catch( error ){ var base = exports; }
-( function module( base ){
-	define( "parseObjectCommandFormat",
-		[
-		],
-		function construct( ){
-			var parseObjectCommandFormat = function parseObjectCommandFormat( data ){
-				
-			};
+/*:
+	@module-configuration:
+		{
+			"packageName": "parse-object-command-format",
+			"fileName": "parse-object-command-format.js",
+			"moduleName": "parseObjectCommandFormat",
+			"authorName": "Richeve S. Bebedor",
+			"authorEMail": "richeve.bebedor@gmail.com",
+			"repository": "git@github.com:volkovasystems/parse-object-command-format.git"
+		}
+	@end-module-configuration
 
-			base.parseObjectCommandFormat = parseObjectCommandFormat;
+	@module-documentation:
+		
+	@end-module-documentation
+
+	@include:
+	@end-include
+*/
+var parseObjectCommandFormat = function parseObjectCommandFormat( stringData ){
+	//This will separate and extract the object command format tokens.
+	var matchList = stringData.match( OBJECT_COMMAND_FORMAT_GREEDY_PATTERN );
+
+	//This will remove excess tokens that is not needed.
+	matchList = matchList.join( "[\n]" ).replace( /\t/g, "" ).split( "[\n]" );
+
+	//We need to traverse each object command data to parse JSON data parameters.
+	var objectCommandList = [ ];
+	var matchListLength = matchList.length;
+	var objectCommandData;
+	for( var index = 0; index < matchListLength; index++ ){
+		//Remove excess spaces at both ends.
+		objectCommandData = matchList[ index ].trim( );
+		
+		//Extract parameter data and command.
+		objectCommandData = objectCommandData.match( OBJECT_COMMAND_FORMAT_PATTERN );
+
+		//This is the only hack I can think to separate JSON format strings.
+		var parameterData = objectCommandData[ 2 ];
+		try{
+			parameterData = JSON.parse( parameterData );
+		}catch( error ){
+			//Remove excess whitespaces for non-JSON strings.
+			parameterData = parameterData.replace( /^\s+/gm, "" );
+		}
+
+		objectCommandList.push( {
+			"command": objectCommandData[ 1 ],
+			"parameterData": parameterData
 		} );
-} )( base );
+	}
+
+	return objectCommandList;
+};
+
+const OBJECT_COMMAND_FORMAT_GREEDY_PATTERN = /(?:@([a-z][a-z0-9]+(?:-[a-z][a-z0-9]+)*):)\s*([^\b]+?)\s*(?:@end-(?:\1|command))/gm;
+const OBJECT_COMMAND_FORMAT_PATTERN = /(?:@([a-z][a-z0-9]+(?:-[a-z][a-z0-9]+)*):)\s*([^\b]+?)\s*(?:@end-(?:\1|command))/;
+
+( module || { } ).exports = parseObjectCommandFormat;
